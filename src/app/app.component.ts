ed68b6e56269e920f8825d0608bd6e7c13e6b6d7
Ramon Hernandez <eldiaadia24h@gmail.com>
2020-04-27 20:32:22 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+declare function init_plugins();
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'zd';
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    init_plugins();
+  }
+  title = "zd";
 }
